7ded35ce910827f112e99562a8027dea5b40ecbc
shiro615 <ys2014hs@gmail.com>
2015-02-12 12:40:36 +0900

    db/fixtures/restaurants.rb作成

---
+++ db/fixtures/restaurants.rb
@@ -0,0 +1,20 @@
+Restaurant.seed do |s|
+  s.tabelog_url = "http://tabelog.com/tokyo/A1301/A130102/13043018/"
+  s.name = "有楽町・日比谷"
+  s.title = "ザ・ロビー"
+  s.description = "洋食・欧風料理"
+  s.address = "東京都千代田区有楽町1-8-1 ザ・ペニンシュラ東京１Ｆ"
+  s.lat = 35.674699
+  s.lng = 139.760658
+end
+
+Restaurant.seed do |s|
+  s.tabelog_url = "http://tabelog.com/tokyo/A1301/A130102/13043018/"
+  s.name = "有楽町・日比谷"
+  s.title = "JSレネップ"
+  s.description = "ドイツ料理"
+  s.address = "東京都千代田区有楽町2-1-8"
+  s.lat = 35.672914
+  s.lng = 139.760601
+end
+
